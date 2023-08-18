d8f6b7f2346cd0b06f8515acf38630f6e14d0781
yomarcoslinss <vinicius.gzm0@gmail.com>
2023-08-18 00:44:05 -0300

    Aula 'Campo de texto' completa

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './index.css';
-import Banner from './components/Banner/Banner'
+import Banner from './components/Banner';
+import CampoTexto from './components/CampoTexto';
 
 function App() {
   return (
     <div className="App">
       <Banner />
+      <CampoTexto />
 
     </div>
   );
