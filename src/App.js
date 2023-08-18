87db50c3473f5feeaaa47fe8772f8b5e5b40eb43
yomarcoslinss <vinicius.gzm0@gmail.com>
2023-08-18 00:07:36 -0300

    Aula 'Explorando o projeto' completa

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
 import Banner from './components/Banner/Banner'
 
 function App() {
   return (
     <div className="App">
       <Banner />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Bem vindo ao React xD
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          React eh vida!
-        </a>
-      </header>
+
     </div>
   );
 }
--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -3,7 +3,7 @@ import './Banner.css'
 function Banner() {
     return (
         <header className="banner">
-        <img src="./images/banner.png" alt="Banner principal da página"/>
+            <img src="./images/banner.png" alt="Banner principal da página"/>
         </header>
     )
 }
