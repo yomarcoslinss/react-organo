26d16fd217d78949f3f4d71c23e8a468dd93584d
yomarcoslinss <vinicius.gzm0@gmail.com>
2023-09-19 22:00:18 -0300

    Aula 'Criando o botão' completa

--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -1,6 +1,7 @@
 import './Formulario.css';
 import CampoTexto from '../CampoTexto';
 import ListaSuspensa from '../ListaSuspensa';
+import Botao from '../Botao';
 
 const Formulario = () => {
 
@@ -16,6 +17,9 @@ const Formulario = () => {
                 <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
                 <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
                 <ListaSuspensa label="Time" itens={times}/>
+                <Botao>
+                    Criar Card
+                </Botao>
             </form>
         </section>
     );
