720becc9caac2289e0badbe6dfe8e5bb62290ecf
yomarcoslinss <vinicius.gzm0@gmail.com>
2023-09-21 20:40:52 -0300

    Aula 'Manipulando o input do usuario' completa

--- src/components/CampoTexto/index.js
+++ src/components/CampoTexto/index.js
@@ -1,16 +1,27 @@
-import './CampoTexto.css'
+import "./CampoTexto.css";
 
 const CampoTexto = (props) => {
+    const placeholderModificada = `${props.placeholder}...`;
+    
+    let valor = 'Marcos Linss';
 
-  const placeholderModificada = `${props.placeholder}...`;
+    const aoDigitar = (e) => {
+        valor = e.target.value;
+        console.log(valor);
+    }
 
 
     return (
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input required={props.obrigatorio} placeholder={placeholderModificada} />
+            <input
+                value = {valor}
+                onChange = {aoDigitar}
+                required = {props.obrigatorio}
+                placeholder = {placeholderModificada}
+            />
         </div>
-    )
-}
+    );
+};
 
 export default CampoTexto;
